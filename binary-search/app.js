6950293b7d892442b023e8d696bb97bd4e03c6a2
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2022-11-11 16:34:51 +0600

    binary search and array indexOf speed comparison

--- binary-search/app.js
+++ binary-search/app.js
@@ -1,20 +1,47 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const target = 4
+const arr = new Array(100000).fill(1).map((_, index)=> index)
+const target = 10000
 
+function binary(arr, target) {
+    let index = -1
 
-let mid = Math.round(arr.length / 2);
+    let mid = Math.round(arr.length / 2);
 
-while(true){    
-    if(arr[mid] === target){
-        console.log("result found index "+mid + " and result "+arr[mid]);
-        break;
-    } else if(target > arr[mid]){
-        mid = mid + 1
-    } else if(target < arr[mid]){
-        mid = mid - 1
-    } else{
-        console.log("not match");
-        break;
+    while (true) {
+        if (arr[mid] === target) {
+            // console.log("result found index "+mid + " and result "+arr[mid]);
+            index = mid
+            break;
+        } else if (target > arr[mid]) {
+            mid = mid + 1
+        } else if (target < arr[mid]) {
+            mid = mid - 1
+        } else {
+            // console.log("not match");
+            break;
 
+        }
     }
+
+    return index;
 }
+
+function find(arr, target) {
+    return arr.indexOf(target)
+}
+
+console.time("binary-search")
+console.log(binary(arr, target));
+console.timeEnd("binary-search")
+
+// 10000
+// binary-search: 10.06ms
+
+
+console.time("arr find")
+console.log(find(arr, target));
+console.timeEnd("arr find")
+// 10000
+// arr find: 0.288ms
+
+
+
