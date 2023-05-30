ef26f35b68837ab88ba1e20bde031bb0fbb4cf4a
Rasel Mahmud <rasel.mahmud.dev@gmail.com>
2023-05-30 14:00:29 +0600

    update stack ds readme

--- stack/index.js
+++ stack/index.js
@@ -1,22 +1,40 @@
 
 // stack data structure implements using JavaScript. First In Last Out. 
 
-class Stack{
-  lastIndex=0
-  items=[]
-  get(){
+class Stack {
+  lastIndex = 0
+  size = 0
+  items = []
+
+  get() {
+    // get array last element.
     let el = this.items[this.lastIndex - 1]
-    this.items.splice(this.lastIndex-1,1)
+
+    // decrease index to get access stack down element.
+    this.lastIndex-- 
     return el
   }
-  add(elm){
+
+  add(elm) {
     this.items.push(elm)
+    // store lastIndex for get last element.
     this.lastIndex++
+
+    // also increase stack size. 
+    this.size++
   }
 }
+
 
 let s = new Stack()
 s.add(12)
 s.add(13)
+s.add(1000)
+
 console.log(s.get())
+console.log(s.get())
+console.log(s.get())
+
+
+
 console.log(s.items)
---
+++ stack/readme.md
@@ -0,0 +1,15 @@
+## Stack
+
+Stack data structure implementation with javascript.
+
+Stack is a linear data structure that used for execute javascript main thread all task known as call stack. 
+
+It perform operation as (FILO: Fast in Last Out) order.
+
+Like a Wear drawer. where store
+
+
+
+
+
+ 
